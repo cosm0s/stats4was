4802ed2fcd582f65db086242bf556998a5b73654
cosm0s <apascualco@gmail.com>
2015-11-17 21:06:08 +0100

    [Main] Added

--- src/main/java/cosm0s/stats4was/core/Stats4Was.java
+++ src/main/java/cosm0s/stats4was/core/Stats4Was.java
@@ -2,9 +2,14 @@ package cosm0s.stats4was.core;
 
 import cosm0s.stats4was.core.connector.ManagementConnection;
 import cosm0s.stats4was.core.exception.Stats4WasException;
+import cosm0s.stats4was.core.stats.ListAttributes;
+import cosm0s.stats4was.core.stats.ListBeans;
+import cosm0s.stats4was.core.stats.ListOperation;
+import cosm0s.stats4was.core.stats.OptionLauncher;
+import cosm0s.stats4was.core.stats.ThreadManager;
 import cosm0s.stats4was.log.L4j;
 import cosm0s.stats4was.utils.CommonsCLI;
-
+import cosm0s.stats4was.utils.Constants;
 
 import java.util.Map;
 
@@ -20,19 +25,27 @@ public class Stats4Was {
             if(options.get("option") != null){
                 option = options.get("option");
             }
-            if(option == null){
-
+            OptionLauncher optionLauncher = null;
+            if(option == null || option.isEmpty()){
+                optionLauncher = new ThreadManager();
             } else if("lb".equals(option.toLowerCase())){
-
+                optionLauncher = new ListBeans();
             } else if("lo".equals(option.toLowerCase())){
-
+                optionLauncher = new ListOperation();
             } else if ("la".equals(option.toLowerCase())){
-
+                optionLauncher = new ListAttributes();
+            } else if ("v".equals(option.toLowerCase())){
+                L4j.getL4j().info(Constants.appName + " v" + Constants.version);
             }
+            if(optionLauncher != null) {
+                L4j.getL4j().debug("Set management connection and start");
+                optionLauncher.setManagementConnection(managementConnection);
+                optionLauncher.start();
+            }
+            L4j.getL4j().info("Leaving stats4was...");
 
         } catch (Stats4WasException ex) {
             L4j.getL4j().error("Parsing arguments", ex);
         }
-
     }
 }
