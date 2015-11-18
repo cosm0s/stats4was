61444eaaf6048159b4b9fed40d2a32a708ecbba6
cosm0s <apascualco@gmail.com>
2015-11-18 22:04:19 +0100

    [JUnit] UtilsFile test

---
+++ src/test/java/UtilsFileTest.java
@@ -0,0 +1,44 @@
+import cosm0s.stats4was.utils.UtilsFile;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class UtilsFileTest {
+
+    @Test
+    public void checkFileTest(){
+
+        String properties = "resources/conf/soap.properties";
+        String stats4was = "resources/conf/stats4was.properties";
+
+        int valorEntero = (int) Math.floor(Math.random()*(0-1000000+1)+1000000);
+        String falseProperties = "C://props"+ valorEntero +".properties";
+
+        assertTrue(UtilsFile.checkFile(properties));
+        assertTrue(UtilsFile.checkFile(stats4was));
+        assertFalse(UtilsFile.checkFile(falseProperties));
+
+        List<String> testList = new LinkedList<String>();
+        testList.add(properties);
+        testList.add(stats4was);
+        testList.add(falseProperties);
+
+        assertFalse(UtilsFile.checkFile(testList));
+
+        testList.remove(falseProperties);
+
+        assertTrue(UtilsFile.checkFile(testList));
+
+
+    }
+
+    @Test(expected=NullPointerException.class)
+    public void testPrintNull(){
+        UtilsFile.print("", null);
+    }
+
+}
