f630959fb71bce82824d2903a9e246959dc63f28
cosm0s <apascualco@gmail.com>
2015-11-21 23:04:49 +0100

    [StatisticsReader] Added the class that read statistics in wstatic

--- src/main/java/cosm0s/stats4was/core/statistics/StatisticsReader.java
+++ src/main/java/cosm0s/stats4was/core/statistics/StatisticsReader.java
@@ -1,4 +1,21 @@
 package cosm0s.stats4was.core.statistics;
 
+import com.ibm.websphere.pmi.stat.WSStats;
+
+import javax.management.ObjectName;
+import java.util.List;
+
 public class StatisticsReader {
+
+    private ObjectName perfBean;
+    private WSStats wsStats;
+
+    public StatisticsReader(ObjectName perfMBean, WSStats wsStats){
+        this.perfBean = perfMBean;
+        this.wsStats = wsStats;
+    }
+
+    public List<String> parser(){
+        return null;
+    }
 }
