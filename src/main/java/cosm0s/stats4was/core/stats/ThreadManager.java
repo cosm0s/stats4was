07cdefa8fb6a67b765d008c55e0ad7befebc8499
cosm0s <apascualco@gmail.com>
2015-11-21 23:06:48 +0100

    [TreadManager] Launch the basic treads that get statistics and send it

--- src/main/java/cosm0s/stats4was/core/stats/ThreadManager.java
+++ src/main/java/cosm0s/stats4was/core/stats/ThreadManager.java
@@ -1,23 +1,58 @@
 package cosm0s.stats4was.core.stats;
 
+import com.ibm.websphere.management.AdminClient;
+import com.ibm.websphere.management.exception.ConnectorException;
+import com.ibm.websphere.management.exception.ConnectorNotAvailableException;
 import cosm0s.stats4was.core.connector.ManagementConnection;
 import cosm0s.stats4was.core.threads.StatsCollector;
+import cosm0s.stats4was.core.threads.observer.ThreadObserver;
 import cosm0s.stats4was.log.L4j;
 import cosm0s.stats4was.utils.DaemonContext;
+import cosm0s.stats4was.utils.MBeansUtils;
 
+import javax.management.ObjectName;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadManager implements OptionLauncher {
 
     private ManagementConnection managementConnection;
-    private List<StatsCollector> threads;
+    private AdminClient adminClient;
+    private boolean started;
+    private long intervalInMili;
 
     @Override
     public void start() {
+        this.init();
+        while(this.started){
+            try {
+                long elapsetStart = System.currentTimeMillis();
+                checkConnections();
+                this.LaunchWorkers(this.createStatsCollectors());
+                long delay = (System.currentTimeMillis() - elapsetStart);
+                Thread.sleep(this.intervalInMili-delay);
+            } catch (InterruptedException e) {
+                this.started = false;
+                L4j.getL4j().error("TreadManager sleep error: ", e);
+            }
+        }
+    }
+
+    public void init(){
         this.managementConnection.connect();
-        this.threads = this.createThreads();
-        System.out.println(this.threads.size());
+        this.adminClient = this.managementConnection.getConnector().getAdminClient();
+        this.started = true;
+        try {
+            this.intervalInMili = Long.valueOf(DaemonContext.instance().getProperty("ElapsedThreadinterval")) * 1000;
+            L4j.getL4j().debug("Elapsed thread interval: " + this.intervalInMili/1000 + " seconds");
+        } catch (NumberFormatException ex) {
+            //DEFAULT
+            this.intervalInMili = 60000;
+        }
     }
 
     @Override
@@ -25,8 +60,7 @@ public class ThreadManager implements OptionLauncher {
         this.managementConnection = managementConnection;
     }
 
-    private List<StatsCollector> createThreads() {
-        List<StatsCollector> statsCollectors = new LinkedList<StatsCollector>();
+    private void LaunchWorkers(List<StatsCollector> statsCollectors){
         //Default
         int maxThreads = 5;
         try {
@@ -34,11 +68,48 @@ public class ThreadManager implements OptionLauncher {
         } catch(NumberFormatException ex) {
             L4j.getL4j().info("Format error in MaxThreads conf");
         }
+        ExecutorService executor = Executors.newFixedThreadPool(maxThreads);
+        for(StatsCollector statsCollector:statsCollectors){
+            Runnable worker = statsCollector;
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        try {
+            if(executor.awaitTermination(Long.valueOf(DaemonContext.instance().getProperty("ElapsedThreadinterval")), TimeUnit.SECONDS)){
+                L4j.getL4j().debug("IN TIME");
+            } else {
+                L4j.getL4j().info("OVER TIME");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<StatsCollector> createStatsCollectors() {
+        List<StatsCollector> statsCollectors = new LinkedList<StatsCollector>();
+        Set<ObjectName> webSphereRuntime = MBeansUtils.getAllServerRuntimes(this.adminClient);
+        int threadCount = 1;
+        for(ObjectName objectName: webSphereRuntime) {
+            String node = objectName.getKeyProperty("node");
+            if(!"dmgr".equals(node)) {
+                StatsCollector statsCollector = new StatsCollector(objectName.getKeyProperty("name"), node, threadCount++);
+                statsCollector.setManagementConnection(this.managementConnection);
+                ThreadObserver threadObserver = new ThreadObserver();
+                statsCollector.addObserver(threadObserver);
+                statsCollectors.add(statsCollector);
+            }
+        }
         return statsCollectors;
     }
 
-
-
-
+    public void checkConnections(){
+        try {
+            this.adminClient.isAlive();
+        } catch (ConnectorException e) {
+            if(e instanceof ConnectorNotAvailableException){
+                this.managementConnection.connect();
+            }
+        }
+    }
 
 }
--- src/main/java/cosm0s/stats4was/core/threads/StatsCollector.java
+++ src/main/java/cosm0s/stats4was/core/threads/StatsCollector.java
@@ -1,11 +1,53 @@
 package cosm0s.stats4was.core.threads;
 
+import com.ibm.websphere.pmi.stat.WSStats;
+import cosm0s.stats4was.core.connector.ManagementConnection;
+import cosm0s.stats4was.core.statistics.StatisticsReader;
+import cosm0s.stats4was.log.L4j;
+import cosm0s.stats4was.sender.Sender;
+import cosm0s.stats4was.utils.MBeansUtils;
+
+import javax.management.ObjectName;
+import java.util.List;
 import java.util.Observable;
 
 public class StatsCollector extends Observable implements Runnable {
 
+    private StatisticsReader statisticsReader;
+    private Sender sender;
+    private int threadsNumber;
+    private String name;
+    private String node;
+    private ManagementConnection managementConnection;
+
+
+    public StatsCollector(String name, String node, int threadsNumber){
+        this.threadsNumber = threadsNumber;
+        this.name = name;
+        this.node = node;
+    }
+
     @Override
     public void run() {
+        Thread.currentThread().setName("Stats4Was-Thread-" + threadsNumber);
+        L4j.getL4j().debug("Start thread: " + Thread.currentThread().getName() + " connection state: " + this.managementConnection.getConnector().getState());
+        getStatistics();
+        L4j.getL4j().debug("End thread: " + Thread.currentThread().getName() + " connection state: " + this.managementConnection.getConnector().getState());
+    }
 
+    public void setSender(Sender sender) {
+        this.sender = sender;
+    }
+
+    public void setManagementConnection(ManagementConnection managementConnection) {
+        this.managementConnection = managementConnection;
+    }
+
+    public List<String> getStatistics() {
+        ObjectName perfMBean = MBeansUtils.getPerfBean(this.managementConnection.getConnector().getAdminClient(), this.node, this.name );
+        L4j.getL4j().debug("Perf Bean: " + String.valueOf(perfMBean));
+        WSStats wsStats = MBeansUtils.getWSStats(this.managementConnection.getConnector().getAdminClient(), this.node, this.name);
+        StatisticsReader statisticsReader = new StatisticsReader(perfMBean, wsStats);
+        return statisticsReader.parser();
     }
 }
